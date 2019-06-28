1db983ce402f28cae5e1a9836fda20d2727977fd
snrndi121 <snrndi4521@gmail.com>
2019-06-28 15:54:29 +0900

    front : android_aws 연결

--- category/frontend/kbas/app/src/main/java/com/kbas/MainActivity.java
+++ category/frontend/kbas/app/src/main/java/com/kbas/MainActivity.java
@@ -1,15 +1,61 @@
 package com.kbas;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
-
+    //aws-commu
+    private final String mServerPublicIp="13.125.216.41";
+    private final String mServerPort="443";
+    private Socket mSocket;
+    //mainactivity
+    private final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        setConfig();
     }
+    public void setConfig() {
+        try {
+            mSocket = IO.socket(mServerPublicIp +":" + mServerPort);
+            mSocket.connect();
+            mSocket.on(Socket.EVENT_CONNECT, onConnect);
+            mSocket.on("serverMessage", onMessageReceived);
+        } catch(URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+    // Socket서버에 connect 되면 발생하는 이벤트
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            mSocket.emit("clientMessage", "hi");
+        }
+    };
+
+    // 서버로부터 전달받은 'chat-message' Event 처리.
+    private Emitter.Listener onMessageReceived = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            // 전달받은 데이터는 아래와 같이 추출할 수 있습니다.
+            try {
+                JSONObject receivedData = (JSONObject) args[0];
+                Log.d(TAG, receivedData.getString("msg"));
+                Log.d(TAG, receivedData.getString("data"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 }
